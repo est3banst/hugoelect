17fca8f62dc0b2e8e24ac5635b27ea15eee87858
est3banst <estebeeen@outlook.com>
2024-02-03 00:38:16 -0300

    fixed some more

--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -12,7 +12,8 @@ export default function Nav() {
     
     return (
         <>
-         <div className='title-container'>
+         <header className='title-container'>
+         <div className='header-container'>
         <h1 className='heading-salto'>
           SALTO INSTALACIONES
         </h1>
@@ -21,30 +22,36 @@ export default function Nav() {
       <span className='logo-container'>
         <img src='/imgs/saltologo.png' alt='brand logo' className='saltologo' />
       </span>
-      </div>
-       <div className='nav-container'>
-       <div id="nav-icon1" className={menuIco} onClick={settingOpen}>
+       </div>
+       
+        <div className='header-icons'>
+        <div id="nav-icon1" className={menuIco} onClick={settingOpen}>
             <span></span>
             <span></span>
             <span></span>
-        <div className='social-nav-icons'>
-            <svg className='social-icon' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2"
+            </div>
+
+            <span id="insta">
+            <svg className='header-social-icon' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2"
                  stroke="currentColor" fill="none" strokeLinecap="round" 
                  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                  <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 
                  0 1 -4 -4z" />
                  <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M16.5 7.5l0 .01" />
             </svg>
-            <svg className='social-icon' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
+            </span>
+            <span id="face">
+            <svg className='header-social-icon' width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
                 stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
             </svg>
+            </span>
             </div>
-        </div>
-       </div>
+           
+      </header>
         
-        <header >
+        <nav >
        
        <ul className={displayMenu}>
         {/* <a href="" className="nav-link-a"> */}
@@ -68,7 +75,9 @@ export default function Nav() {
             </li>
             {/* </a> */}
         </ul>
-        </header>
+        
+        
+        </nav>
         </>
     )
 }
--- src/componentes/Section.jsx
+++ src/componentes/Section.jsx
@@ -1,10 +1,15 @@
+import '../assets/Section.css'
 export default function Section () {
     return (
        <>
        <main className='main-hero-section'>
         <div className='container-section'>
             <img src="/imgs/3.png" alt="" className='img-hero-section' />
+            
             <p>Más de 20 años brindando calidad y tranquilidad a nuestros clientes</p>
+            <a href="">
+                <p>Contacte hoy a un profesional de confianza</p>
+            </a>
         </div>
        </main>
        </> 
