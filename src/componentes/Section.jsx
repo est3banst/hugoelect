173f8c810083c94ede2ee4e725d21925b4f7773b
est3banst <estebeeen@outlook.com>
2024-02-04 10:56:31 -0300

    added hero

--- src/componentes/Section.jsx
+++ src/componentes/Section.jsx
@@ -4,13 +4,26 @@ export default function Section () {
        <>
        <main className='main-hero-section'>
         <div className='container-section'>
-            <img src="/imgs/3.png" alt="" className='img-hero-section' />
+            <img src="/imgs/hugohero.png" alt="" className='img-hero-section' />
+            <span className="section-sliders">
+            <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
+            stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M15 6l-6 6l6 6" />
+            </svg>
+            <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" 
+            stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 6l6 6l-6 6" />
+                </svg>
+            </span>
             
+        <div className="hero-cta">
             <p>Más de 20 años brindando calidad y tranquilidad a nuestros clientes</p>
             <a href="">
                 <p>Contacte hoy a un profesional de confianza</p>
             </a>
         </div>
+            </div>
        </main>
        </> 
     )
