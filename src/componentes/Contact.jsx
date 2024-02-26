9976229b16cf1ffa11f9f285033c21429a7fabd8
est3banst <estebeeen@outlook.com>
2024-02-25 23:50:01 -0300

    added styles to footer

--- src/componentes/Contact.jsx
+++ src/componentes/Contact.jsx
@@ -11,9 +11,6 @@ export default function Contacto () {
         fill="none" /><path d="M12 18h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5" />
     <path d="M3 6l9 6l9 -6" /><path d="M15 18h6" /><path d="M18 15l3 3l-3 3" /></svg>
 
-<h3 className='works-heading'>
-Email
-</h3>
 </a>
 <p className='section-par'>Podes solicitar un presupuesto, o coordinar una visita</p>
 <p className='section-p'>Envianos un mail</p>
@@ -27,7 +24,7 @@ stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1
  -4 4h-8a4 4 0 0 1 -4 -4z" /><path d="M12 12m-3 
 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M16.5 7.5l0 .01" /></svg>      
-<h3 className='works-heading'>Instagram</h3>
+
 </a>
 <p className='section-par'>Tambien podés chequear nuestros trabajos acá</p>
 </div>
@@ -40,11 +37,9 @@ stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
 </svg>
 
-<h3 className='works-heading'>Facebook</h3>
 </a>  
 <p className='section-par'>Podés encontrar más información acá</p>
 </div>
-    
 
 </div>
     )
--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -4,19 +4,21 @@ export default function Footer () {
     return (
         <>
         <footer>
-        <div>
+        <div className='container-footer'>
+           <a href="https://www.google.com/maps/place/Agraciada,+50000+Salto,+Departamento+de+Salto/@-31.3885067,-57.9379319,17z/data=!4m10!1m2!2m1!1sAgraciada!3m6!1s0x95adc32acbacdc4b:0xf9099c5fd75d9aee!8m2!3d-31.3856407!4d-57.952567!15sCglBZ3JhY2lhZGGSAQVyb3V0ZeABAA!16s%2Fg%2F1vlzck54?entry=ttu">
            <small>Salto, Uruguay 
-            <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" 
+           <svg width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5"
                 stroke="currentColor" fill="none" strokeLinecap="round" 
                 strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" 
-                fill="none"/><path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203
-                .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9
-                9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" 
-                stroke-width="0" fill="currentColor" />
-            </svg>
+                fill="none"/><path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                <path d="M12.02 21.485a1.996 1.996 0 0 1 -1.433 -.585l-4.244
+                -4.243a8 8 0 1 1 13.403 -3.651" />
+            <path d="M16 22l5 -5" /><path d="M21 21.5v-4.5h-4.5" />
+           </svg>
             </small>
+           </a>
         </div>
-        <div>
+        <div className='second-container-footer'>
             <small>Horario de atención: Lunes a Viernes de 8:00 a 18:00</small>
         <h3>SALTO INSTALACIONES</h3>
         </div>
