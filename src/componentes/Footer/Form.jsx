1ceb912ac59f6be2d0af15e3e288b34867d716ef
est3banst <estebeeen@outlook.com>
2024-02-27 20:39:02 -0300

    added form funct

--- src/componentes/Footer/Form.jsx
+++ src/componentes/Footer/Form.jsx
@@ -1,26 +1,63 @@
 import '../../assets/Form.css'
+import { useForm, ValidationError } from '@formspree/react'
+import { API_KEY } from '../data/form'
 
 export default function Form() {
+    const [ state, handleSubmit ] = useForm(API_KEY);
+    if (state.succeeded) {
+        return <p>Gracias por contactarnos, nos comunicaremos a la brevedad!</p>
+    }
     return (
         <div className="container">
-        <form className="form">
+        <form onSubmit={handleSubmit} className="form">
           <div className="descr">Dejanos tu mensaje!</div>
 
           <div className="input">
-              <input required="" autoComplete="off" type="text" />
+              <input 
+              required='' 
+              autoComplete='off' 
+              type='text'
+              id='name'
+              name='name' />
               <label htmlFor="name">Nombre</label>
+              <ValidationError 
+                prefix="Name" 
+                field="name"
+                errors={state.errors}
+              />
           </div>
     
-          <div className="input">
-              <input required="" autoComplete="off" name="email" type="text" />
-              <label htmlFor="email">E-mail</label>
+          <div className='input'>
+              <input 
+              required='' 
+              autoComplete='off' 
+              name='email' 
+              type='email'
+              id='email' />
+              <label htmlFor='email'>E-mail</label>
+              <ValidationError 
+                prefix='Email' 
+                field='email'
+                errors={state.errors}
+              />
           </div>
     
-          <div className="input">
-              <textarea required="" cols="30" rows="1" id="message"></textarea>
+          <div className='input'>
+              <textarea required='' 
+              cols='30' 
+              rows='1' 
+              id='message'
+              name='message' >
+
+              </textarea>
               <label htmlFor="message">Mensaje</label>
+              <ValidationError 
+                prefix="Message" 
+                field="message"
+                errors={state.errors}
+              />
           </div>
-          <button>Enviar mensaje →</button>
+          <button type='submit' disabled={state.submitting}>Enviar mensaje →</button>
         </form>
     </div>
     )
